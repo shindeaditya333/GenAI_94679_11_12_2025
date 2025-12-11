12deb2aa241df29cb45862d344b193924b4f1026
Aditya Shinde <shindeaditya9354@gmail.com>
2025-12-11 22:30:02 +0530

    Final Changes

--- Assignments/Day_2/Q3.py
+++ Assignments/Day_2/Q3.py
@@ -20,5 +20,6 @@ for index, row in df.iterrows():
 print("Total number of rows : ",len(df))
 print("Total number of products priced above 500 : \n",df[df['price']>500])
 print("Average price of all products : \n",df['price'].mean())
-print("List all products belonging to a specific category (user input) : \n",df[df['category'] == 'Electronics'])
+cat = input("Enter category: ")
+print("List all products belonging to a specific category (user input) : \n", df[df['category'].str.lower() == cat.lower()])
 print("Total quantity of all items in stock : ", df['quantity'].sum())
