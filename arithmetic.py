f5a11593c29acf9dd018c303386a2b78732c84c1
Aditya Shinde <shindeaditya9354@gmail.com>
2025-12-11 09:38:39 +0530

    Saved Changes

--- arithmetic.py
+++ arithmetic.py
@@ -1,2 +1,5 @@
 def add(a,b):
     print(f"Addition : {a+b}")
+
+def sub(a,b):
+    print(f"Addition : {a-b}")
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import arithmetic as arth
 print("GenAI_94679_11_12_2025")
+a=int(input("Enter value for a : "))
+b=int(input("Enter value for b : "))
+
+
+arth.add(a,b)
+arth.sub(a,b)
